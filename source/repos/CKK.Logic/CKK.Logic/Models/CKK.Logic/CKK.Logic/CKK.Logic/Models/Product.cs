7ad93e325e42a01e5eb9560d5db33427a3ae29b5
ETHAN STRAIN <ETHAN.STRAIN@otech.edu>
2022-04-01 13:19:04 -0600

    test

---
+++ source/repos/CKK.Logic/CKK.Logic/Models/CKK.Logic/CKK.Logic/CKK.Logic/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿namespace CKK.Logic.Models
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private decimal _price;
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public decimal GetPrice()
+        {
+            return _price;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            _price = price;
+        }
+    }
+}
---
+++ source/repos/CKK.Logic/CKK.Logic/Models/Customer.cs
@@ -0,0 +1,51 @@
+﻿namespace CKK.Logic
+{
+    public class Customer
+    {
+
+        // Id int {get; set;}
+        // Name string {get; set;}
+        // Address string {get; set;}
+        private int _Id;
+        private string _Name;
+        private string _Address;
+
+        public Customer(int Id, string Name, string Address)
+        {
+            _Id = Id;
+            _Name = Name;
+            _Address = Address;
+        }
+
+        public int GetId(int Id)
+        {
+            return Id;
+        }
+        public void SetId(int ID)
+        {
+            _Id = ID;
+        }
+
+        public string GetName(string Name)
+        {
+            return Name;
+        }
+
+        public void SetName(string Name)
+        {
+            _Name = Name;
+        }
+
+        public string GetAdress(string Adress)
+        {
+            return Adress;
+        }
+
+        public void SetAddress(string Address)
+        {
+            _Address = Address;
+        }
+
+    }
+
+}
